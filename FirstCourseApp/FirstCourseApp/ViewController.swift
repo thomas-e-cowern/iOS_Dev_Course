839cf6c6a4213fcf31dcc27c445c037fedb33d5c
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-01-30 16:49:37 -0500

    first Commit

---
+++ FirstCourseApp/FirstCourseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstCourseApp
+//
+//  Created by Thomas Cowern New on 1/30/20.
+//  Copyright © 2020 Thomas Cowern New. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
