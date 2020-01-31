76383ba56f230cd070416f54d0bccb5d30138c8e
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-01-30 20:08:10 -0500

    added one way image change

--- FirstCourseApp/FirstCourseApp/ViewController.swift
+++ FirstCourseApp/FirstCourseApp/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeImageButtonPressed(_ sender: Any) {
+        
+        imageView.image = UIImage.init(named: "frank2")
+        
+    }
+    
 }
 
