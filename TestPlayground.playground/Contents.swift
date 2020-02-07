7f83daee3061c7aaf7816ab230b9d6054311e6fe
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-02-07 10:53:55 -0500

    modified test playground

--- TestPlayground.playground/Contents.swift
+++ TestPlayground.playground/Contents.swift
@@ -6,3 +6,12 @@ var four = 4
 var five = 5
 
 print(four + five)
+
+var userName = "Thomas"
+var userSurname = "Cowern"
+
+userName = "Joseph"
+
+let userAge = 50
+
+print("Name: \(userName), Last Name: \(userSurname), Age: \(userAge)")
